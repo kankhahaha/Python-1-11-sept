e2bdc6f067b099397050de99d1a9d153a7b2feb6
Ritvi Mishra <ritvi12@gmail.com>
2020-09-11 17:19:33 +0530

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,23 @@
+your_name = "Jamie"
+
+# Lines 4-5 are a function called 'say_hello'
+def say_hello(name):
+  print("Hey there, {}".format(name))
+  
+# Now we can call the function and substitute any variable as the 'name' parameter!
+say_hello(your_name)
+say_hello("Sam")
+
+
+def multiply_two_numbers(x, y):
+  print(x*y)
+  
+multiply_two_numbers(10,5)
+
+
+
+  
+  
+  
+  
+  
---
+++ variables.py
@@ -0,0 +1,14 @@
+# A variable with an Integer (whole number) value
+
+
+# A variable with a String (text) value
+
+
+# We can print out the content of variables in a couple of ways:
+
+      
+      
+      
+      
+      
+      
